a2ac10ab9efa677b7560ffd47e7b80f0ea424b55
davidra <davdragon@gmail.com>
2022-10-19 10:52:35 +0300

    first push of new repo

---
+++ debug_trials.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+#import webcolors
+import matplotlib.colors as mc
+import time
+from PIL import Image, ImageOps
+#from keras.models import load_model
+import pyzbar.pyzbar as pyzbar
+# import tensorflow.python
+from os import listdir
+from os.path import isfile, join
+from os.path import exists
+import os
+import sys
+import argparse
+import pytesseract
+from djitellopy import Tello
+from DetectShapes.detect_shapes_OpenCV import DetectUtils
+from DetectShapes.detect_shapes_OpenCV import DetectScreenInFrame
+import logging
+import threading
+from Main_Tello_Simon import TelloControl
+
+
+timing_img = 10
+dirpath = r"C:\Users\davidra\Desktop\NINJA_RAFAEL\Ninja_Symon2\first_try8"
+filename = "frame_0_Screen detected, screen mode is 2" + ".jpg"
+full_path = os.path.join(dirpath, filename)
+img = cv2.imread(full_path)
+cv2.imshow("photo", img)  # optional display photo
+cv2.waitKey(0)
+debugTello = TelloControl(dirpath=dirpath, debug=True)
+debugTello.ImageProcessing.original_image = img
+
+debugTello.ImageProcessing.DetectScreen(True)
+#debugTello.AnalyzePicture()
